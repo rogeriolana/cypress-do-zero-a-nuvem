34ab7c9440dbd517b3f9ab610b882fb97fb338ac
rogerio lana <rogeriolana9@gmail.com>
2025-05-28 20:10:18 -0300

    Quarto commit

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -21,4 +21,40 @@ describe('Central de atendimento ao cliente TAT', () => {
     cy.get('.success').should('be.visible') 
     
 })
+
+it('Exibe mensagem de erro ao submeter o formulario em email com formatação invalida', () => {
+    cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('rogeriolana9@gmail,com')
+    cy.get('#open-text-area').type('Teste ')
+    cy.get('button[type="submit"]').click()
+    
+    cy.get('.error').should('be.visible') 
+
+  
+});
+
+it('Campo telefone continua vazio quando preenchido com valor nao-numerico', () => 
+  {
+    cy.get('#phone')
+      .type('abcdefghij')
+      .should('have.value', '')
+      
+  
+});
+
+it('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+     cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('rogeriolana9@gmail.com')
+    cy.get('#open-text-area').type('Teste ')
+    cy.get('#phone-checkbox').click()
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+
+})
+
+
+
 })
